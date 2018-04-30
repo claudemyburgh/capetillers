37628c3279fc868e25596b651b16a1cd578e460c
Claude Myburgh <cluade@designbycode.co.za>
2018-04-30 04:23:19 +0200

    tiller video added

--- resources/views/videos/index.blade.php
+++ resources/views/videos/index.blade.php
@@ -19,10 +19,18 @@
 <div class="wrapper  margin--bottom-small">
 	<div class="row">
 		<div class="md-col-8 md-offset-2 text-align-center">
+			<h2>Tiller with Plough</h2>
+			<div class="well ">
+				<div class="responsive__wide-screen">
+
+					<iframe class="responsive__item" width="1280" height="720" src="https://www.youtube.com/embed/cYWMLCeceJE" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+				</div>
+			</div>
+			<div class="devider"></div>
 			<h2>Stalk Harvester</h2>
 			<div class="well ">
 				<div class="responsive__wide-screen">
-					<iframe class="responsive__item" width="560" height="315" src="https://www.youtube.com/embed/vQi5cvIu5tE" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+					<iframe class="responsive__item" width="1280" height="720" src="https://www.youtube.com/embed/vQi5cvIu5tE" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
 				</div>
 			</div>
 			<div class="devider"></div>
